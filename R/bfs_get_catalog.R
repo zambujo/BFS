dc1c5d14a1fb614f815b0a7bc8c2980ce0019e87
João Martins <joao.martins@snf.ch>
2021-04-21 22:48:03 +0200

    refactor: bfs_get_catalog() revision
    Removed superfluous code

--- R/bfs_get_catalog.R
+++ R/bfs_get_catalog.R
@@ -7,7 +7,6 @@
 #' @return A data frame
 #'
 #' @importFrom tidyRSS tidyfeed
-#' @importFrom janitor clean_names
 #' @importFrom dplyr select mutate
 #'
 #' @seealso \code{\link{bfs_get_metadata}}
@@ -17,45 +16,34 @@
 #'
 #' @export
 
-bfs_get_catalog <- function(language = c("de", "fr", "it", "en")) {
+bfs_get_catalog <- function(language) {
   if (missing(language))
-    stop("must choose a language, either 'de', 'fr', 'it' or 'en'", call. = FALSE)
-  
-  language <-
-    match.arg(arg = language, choices = c("de", "fr", "it", "en"))
-  
-  feed <-
-    paste0(
-      "https://www.bfs.admin.ch/bfs/",
-      language,
-      "/home/statistiken/kataloge-datenbanken/daten/_jcr_content/par/ws_catalog.rss.xml?skipLimit=true"
-    )
-  
-  df <- tidyRSS::tidyfeed(feed = feed)
-  
-  colnames(df) <- gsub('feed_', '', colnames(df))
-  colnames(df) <- gsub('item_', '', colnames(df))
-  
-  df <- janitor::clean_names(df, "small_camel")
-  
-  base_url_bfs <-
-    paste0(
-      "https://www.bfs.admin.ch/content/bfs/",
-      language,
-      "/home/statistiken/kataloge-datenbanken/daten.assetdetail."
-    )
-  base_url_px <- "https://www.bfs.admin.ch/bfsstatic/dam/assets/"
-  
+    stop("must choose a language, either 'de', 'fr', 'it' or 'en'", 
+         call. = FALSE)
+  language <- match.arg(arg = language, choices = c("de", "fr", "it", "en"))
+  # url definition  
+  feed <- 
+    "https://www.bfs.admin.ch/bfs/" %+%
+    language %+%
+    "/home/statistiken/kataloge-datenbanken/daten/_jcr_content/par/" %+%
+    "ws_catalog.rss.xml?skipLimit=true"
+  base_url_bfs <- 
+    "https://www.bfs.admin.ch/content/bfs/" %+%
+    language %+%
+    "/home/statistiken/kataloge-datenbanken/daten.assetdetail."
+  base_url_px <- 
+    "https://www.bfs.admin.ch/bfsstatic/dam/assets/"
+  # download feed as tibble -------
+  df <- tidyfeed(feed)
   df <- df %>%
-    dplyr::select(title = title_2,
-                  url_bfs = link_2,
-                  published = pubDate_2) %>%
-    dplyr::mutate(language = language) %>%
-    dplyr::mutate(
-      url_px = gsub(base_url_bfs, base_url_px, url_bfs),
-      url_px = gsub(".html$", "/master", url_px)
-    ) %>%
-    dplyr::select(title, language, published, url_bfs, url_px)
-  
+    mutate(
+      language = language,
+      url_px = gsub(base_url_bfs, base_url_px, item_link),
+      url_px = gsub(".html$", "/master", url_px)) %>%
+    select(title = item_title, 
+           language, 
+           published = item_pub_date, 
+           url_bfs = item_link, 
+           url_px)
   return(df)
 }
